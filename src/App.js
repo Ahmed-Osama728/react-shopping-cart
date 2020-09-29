91cd980ed27dd4bdf208d101989ac170d2bd5601
Ahmed-Osama728 <https://github.com/Ahmed-Osama728/movies-info-app>
2020-09-29 03:55:47 +0200

    checkout-form

--- src/App.js
+++ src/App.js
@@ -11,14 +11,23 @@ class App extends React.Component {
       products: data.products,
       size: '',
       sort: '',
-      cartItems: [],
+      cartItems: localStorage.getItem('cartItem')
+        ? JSON.parse(localStorage.getItem('cartItem'))
+        : [],
     };
   }
+  createOrder = (order) => {
+    alert(`create order for ${order.name}`);
+  };
   removeFromCart = (product) => {
     const cartItems = this.state.cartItems.slice();
     this.setState({
       cartItems: cartItems.filter((x) => x._id !== product._id),
     });
+    localStorage.setItem(
+      'cartItem',
+      JSON.stringify(cartItems.filter((x) => x._id !== product._id))
+    );
   };
   addToCart = (product) => {
     const cartItems = this.state.cartItems.slice();
@@ -33,6 +42,7 @@ class App extends React.Component {
       cartItems.push({ ...product, count: 1 });
     }
     this.setState({ cartItems });
+    localStorage.setItem('cartItem', JSON.stringify(cartItems));
   };
   sortProducts = (event) => {
     // impl
@@ -96,6 +106,7 @@ class App extends React.Component {
               <Cart
                 cartItems={this.state.cartItems}
                 removeFromCart={this.removeFromCart}
+                createOrder={this.createOrder}
               />
             </div>
           </div>
